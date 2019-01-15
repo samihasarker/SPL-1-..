8a665adef376c1f7c340d9784c453dbd2cf560e6
samihasarker <35795402+samihasarker@users.noreply.github.com>
2019-01-15 11:54:57 +0600

    Add files via upload
    learning python

---
+++ test4C2.py
@@ -0,0 +1,12 @@
+print("If a 2000 pound pregnant hippo gives birth to a 10 pound calf.")
+print("but then eats 50 pounds of food , how much does she weight?")
+input("press the enter key to find out")
+print("2000 -100 + 50 =", 2000-100+50)
+
+
+print("\nIf that same group of 4 pirates evenly divides the chest full")
+print("of 107 gold coins, how many coins are left ever ?")
+input("press the enter key to find out")
+print("107 % 4=",107 % 4)
+
+input("\n\npress th enter key to exit.")
