1d0e4dd9898037be0bcc38cb1218272ef229857c
samihasarker <35795402+samihasarker@users.noreply.github.com>
2019-01-15 11:52:11 +0600

    Add files via upload
    learning python

---
+++ forloop7.py
@@ -0,0 +1,16 @@
+'''data = [3, 45, 56, 78, 54, 56]
+print(data)'''
+
+
+
+'''data = [23, 45, 66, 89, 90, 99]
+i = 0
+while i<=5:
+    print(data[i])
+    i+=1'''
+
+
+
+data = [23, 45, 67, 87, 99, 900]
+for x in data:
+    print(x)
