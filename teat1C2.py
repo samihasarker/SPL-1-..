e1b86935e90b36d1b469e45b3c4420549e00f0d0
samihasarker <35795402+samihasarker@users.noreply.github.com>
2019-01-15 11:53:37 +0600

    Add files via upload
    learning python

---
+++ teat1C2.py
@@ -0,0 +1,10 @@
+print("program 'game over '")
+print("same ", "message", "as before")
+print("just",
+      "a bit",
+      "bigger")
+
+print("here", end="")
+print("\nit is ...")
+
+print("\n\npress the enter key to exit.")
