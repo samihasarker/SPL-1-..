47292793a0c0fe9982854e0e96e101e166880fb1
samihasarker <35795402+samihasarker@users.noreply.github.com>
2019-01-15 11:54:29 +0600

    Add files via upload
    learning python

---
+++ test3C2.py
@@ -0,0 +1,10 @@
+print("you can concatenate two" + "strings with the  '+' operator")
+
+print("\nthis string " + "may not " + "seem terr" + "ibly impressive."\
+      +"1" + "y" + "string is now over")
+
+print("\nIf you really like a string,you can repeat it,for example.")
+print("like it ,you should say it like you mean it:")
+print("pie" * 10)
+
+input("\n\npress the enter key to exit.")
