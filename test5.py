c80cf7ac2f56794532d377fd18eb29c429b8c8e7
samihasarker <35795402+samihasarker@users.noreply.github.com>
2019-01-15 11:48:32 +0600

    Add files via upload
    learning python

---
+++ test5.py
@@ -0,0 +1,20 @@
+'''i = 1
+while i<= 10:
+    print("i love python")
+    i = i+1'''
+
+
+'''i = 1
+while i <= 100:
+    print(i)
+    i += 1'''
+
+'''i = 2
+while i<=100:
+    print(i)
+    i+=2'''
+
+i = 1
+while i<=100:
+    print(i)
+    i+=2
