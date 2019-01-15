7c61f2787123be56d4078c98613c8220c24fefcf
samihasarker <35795402+samihasarker@users.noreply.github.com>
2019-01-15 11:53:59 +0600

    Add files via upload
    learning python

---
+++ test2C2.py
@@ -0,0 +1,14 @@
+print("\t\t\tFancy credits ")
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ ")
+print("\t\t\tby")
+print("\t\t\t\tMichael Dawson")
+print("\t\t\t\t \\ \\ \\ \\")
+
+print("\nSpecial thanks goes out to: ")
+print("My hair stylish,honey \'the great\',who never says\" can\'t ")
+      
+#sound the system bell
+print("\a")
+
+input("\n\npress the enter key to exit.")
