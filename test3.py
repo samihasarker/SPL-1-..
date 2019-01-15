13aae4ad44cdaeb0c95a673e56b8f8da7eb62610
samihasarker <35795402+samihasarker@users.noreply.github.com>
2019-01-15 11:45:35 +0600

    Add files via upload
    learning python

---
+++ test3.py
@@ -0,0 +1,26 @@
+myVar = '2+2'
+print(myVar)
+
+
+myVar2 = 2+2
+print(myVar2)
+
+
+var = ("hello ,i\'m samiha")
+print(var)
+
+
+test = ("two + three makes  ")
+math = 2 + 3
+result = str(math)
+print(test +  result)
+
+x,y,z = ( 3 ,4 ,5 )
+print(x)
+print(y)
+print(z)
+
+
+
+
+
